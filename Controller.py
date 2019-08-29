b63a22cad1aebe17d4607a1b90f1ac9ae70ae480
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-08-29 13:33:40 +0500

    initial working with bottsstrap settings and basic home page

---
+++ Controller.py
@@ -0,0 +1,17 @@
+import web
+
+urls = (
+    "/", "home"
+)
+app = web.application(urls, globals())
+
+
+# Classes/routes
+
+class home:
+    def GET(self):
+        return "home"
+
+
+if __name__ == "__main__":
+    app.run()
