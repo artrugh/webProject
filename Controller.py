bac7c42421e5eddd899208632c7f770a26518eb9
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-09-04 13:00:47 +0500

    Aded user registration post request

--- Controller.py
+++ Controller.py
@@ -1,8 +1,9 @@
 import web
 
 urls = (
-    "/", "home",
-    "/register", "register"
+    "/", "Home",
+    "/register", "Register",
+    "/postregistration", "PostRegistration"
 )
 render = web.template.render("Views/Templates", base="MainLayout")
 app = web.application(urls, globals())
@@ -10,13 +11,20 @@ app = web.application(urls, globals())
 
 # Classes/routes
 
-class home:
+class Home:
     def GET(self):
         return render.Home()
 
-class register:
+
+class Register:
     def GET(self):
         return render.Register()
+
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+        return data.username
 
 
 if __name__ == "__main__":
--- static/js/scripty.js
+++ static/js/scripty.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
 
     console.log("loaded");
     $.material.init();
-
+    console.log("loaded");
     $(document).on("submit", "#register-form", function(e){
         e.preventDefault();
-        console.log("form submitted");
+
+        var form = $("#register-form").serialize();
+        $.ajax({
+            url: '/postregistration',
+            Type: 'POST',
+            data: form,
+            success: function(response){
+                console.log(response);
+            }
+        });
     });
 
 });
